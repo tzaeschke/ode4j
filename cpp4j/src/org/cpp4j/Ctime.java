15e31f567b4c9103a2d037dc6e8014a8747b8c3c
Til <zoodb@gmx.de>
2010-01-17 15:50:42 +0000

    Updated copyright information.

--- cpp4j/src/org/cpp4j/Ctime.java
+++ cpp4j/src/org/cpp4j/Ctime.java
@@ -1,3 +1,24 @@
+/*************************************************************************
+ *                                                                       *
+ * Open Dynamics Engine 4J, Copyright (C) 2007-2010 Tilmann Zäschke      *
+ * All rights reserved.  Email: ode4j@gmx.de   Web: www.ode4j.org        *
+ *                                                                       *
+ * This library is free software; you can redistribute it and/or         *
+ * modify it under the terms of EITHER:                                  *
+ *   (1) The GNU Lesser General Public License as published by the Free  *
+ *       Software Foundation; either version 2.1 of the License, or (at  *
+ *       your option) any later version. The text of the GNU Lesser      *
+ *       General Public License is included with this library in the     *
+ *       file LICENSE.TXT.                                               *
+ *   (2) The BSD-style license that is included with this library in     *
+ *       the file ODE4J-LICENSE-BSD.TXT.                                 *
+ *                                                                       *
+ * This library is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the files    *
+ * LICENSE.TXT and ODE4J-LICENSE-BSD.TXT for more details.               *
+ *                                                                       *
+ *************************************************************************/
 package org.cpp4j;
 
 public class Ctime extends Ctype {
