d0d44c0eb815be91a1c62221551b4797c6bc3f3d
Til <zoodb@gmx.de>
2012-04-25 19:00:37 +0000

    Fixed compilation errors for Java 7.

--- core/src/org/ode4j/ode/internal/DObject.java
+++ core/src/org/ode4j/ode/internal/DObject.java
@@ -27,14 +27,14 @@ package org.ode4j.ode.internal;
 import org.cpp4j.java.Ref;
 
 
-// base class for bodies and joints
-
+/**
+ * Base class for bodies and joints.
+ */
 public abstract class DObject extends DBase {
 	
 	public DxWorld world;		// world this object is in
 //	  dObject *next;		// next object of this type in list
 //	  dObject **tome;		// pointer to previous object's next ptr
-//	private Ref<dObject> _next;		// next object of this type in list
 	private DObject _next;		// next object of this type in list
 	//private dObject _prev;		// pointer to previous object (TZ)
 //	private dObject tome;		// pointer to previous object's next ptr
@@ -50,27 +50,53 @@ public abstract class DObject extends DBase {
 		tag = 0;
 	}
 	
-	// add an object `obj' to the list who's head pointer is pointed 
-	//to by `first'.
-
+	/**
+	 * Add an object `obj' to the list who's head pointer is pointed 
+	 * to by `first'.
+	 * This is equivalent to obj.addObjectToList(first).
+	 * @param <T> Type of the object/list
+	 * @param obj Object that should be added to a List.
+     * @param first List to which the object should be added.
+	 * @see DObject#addObjectToList(Ref)
+	 */
 	//static void addObjectToList (dObject *obj, dObject **first)
-	public static <T extends DObject> void addObjectToList (T obj, 
-			Ref<T> first)
-	{
-//		System.err.println("ADDING OBJ: " + obj.getClass().getName());
-		//  obj.next = *first;
-		//  obj.tome = first;
-		//  if (first != null) first.tome = &obj.next;
-		//  first = obj;
-		obj._next = first.get();
-		obj._tome = (Ref<DObject>) first;
-		if (first.get() != null) first.get()._tome.set(obj._next);
-		first.set(obj);
-	}
+    public static <T extends DObject> void addObjectToList (T obj, 
+            Ref<T> first)
+    {
+        obj.addObjectToList(first);
+    }
+
+    /**
+     * Add an object `obj' to the list who's head pointer is pointed 
+     * to by `first'.
+     * TODO This is quite weird, having the list function on the obj rather than
+     * on the list...
+     * @param <T> Type of the object/list
+     * @param first List to which the object should be added.
+     */
+    public <T extends DObject> void addObjectToList (Ref<T> first)
+    {
+//      System.err.println("ADDING OBJ: " + obj.getClass().getName());
+        //  obj.next = *first;
+        //  obj.tome = first;
+        //  if (first != null) first.tome = &obj.next;
+        //  first = obj;
+        _next = first.get();
+        _tome = (Ref<DObject>) first;
+        if (first.get() != null) {
+            first.get().setTome(_next);
+        }
+        first.set((T) this);
+    }
+
+    protected void setTome(DObject obj) {
+        _tome.set(obj);
+    }
 
 
-	// remove the object from the linked list
-
+	/**
+	 * Remove the object from the linked list.
+	 */
 //	public static <T extends dObject>void removeObjectFromList (T obj)
 	public <T extends DObject>void removeObjectFromList ()
 	{
