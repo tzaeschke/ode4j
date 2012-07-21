3396500fd8d11d188d30d07e10033b14ff5ee4b3
Til <zoodb@gmx.de>
2012-07-21 15:12:28 +0000

    Fixed DObject and prepared j1.3 release

--- core/src/org/ode4j/ode/internal/DObject.java
+++ core/src/org/ode4j/ode/internal/DObject.java
@@ -31,57 +31,52 @@ import org.cpp4j.java.Ref;
  * Base class for bodies and joints.
  */
 public abstract class DObject extends DBase {
-	
-	public DxWorld world;		// world this object is in
-//	  dObject *next;		// next object of this type in list
-//	  dObject **tome;		// pointer to previous object's next ptr
-	private DObject _next;		// next object of this type in list
-	//private dObject _prev;		// pointer to previous object (TZ)
-//	private dObject tome;		// pointer to previous object's next ptr
-	private Ref<DObject> _tome;		// pointer to previous object's next ptr
-	public int tag;			// used by dynamics algorithms
-	//void userdata;		// user settable data
-	protected Object userdata;		// user settable data
-	protected DObject(DxWorld w) { //From ODE.java
-		world = w;
-		_next = null;
-		_tome = null;
-		userdata = null;
-		tag = 0;
-	}
-	
-	/**
-	 * Add an object `obj' to the list who's head pointer is pointed 
-	 * to by `first'.
-	 * This is equivalent to obj.addObjectToList(first).
-	 * @param <T> Type of the object/list
-	 * @param obj Object that should be added to a List.
+   
+    public DxWorld world;        // world this object is in
+//      dObject *next;        // next object of this type in list
+//      dObject **tome;        // pointer to previous object's next ptr
+    private final Ref<DObject> _next;        // next object of this type in list
+    private Ref<DObject> _tome;        // pointer to previous object's next ptr
+    public int tag;            // used by dynamics algorithms
+    //void userdata;        // user settable data
+    protected Object userdata;        // user settable data
+    protected DObject(DxWorld w) { //From ODE.java
+        world = w;
+        _next = new Ref<DObject>();
+        _tome = null;
+        userdata = null;
+        tag = 0;
+    }
+   
+    /**
+     * Add an object `obj' to the list who's head pointer is pointed
+     * to by `first'.
+     * This is equivalent to obj.addObjectToList(first).
+     * @param <T> Type of the object/list
+     * @param obj Object that should be added to a List.
      * @param first List to which the object should be added.
-	 * @see DObject#addObjectToList(Ref)
-	 */
-	//static void addObjectToList (dObject *obj, dObject **first)
-    public static <T extends DObject> void addObjectToList (T obj, 
-            Ref<T> first)
-    {
+     * @see DObject#addObjectToList(Ref)
+     */
+    //static void addObjectToList (dObject *obj, dObject **first)
+    public static <T extends DObject> void addObjectToList (T obj, Ref<T> first) {
         obj.addObjectToList(first);
     }
 
     /**
-     * Add an object `obj' to the list who's head pointer is pointed 
+     * Add an object `obj' to the list who's head pointer is pointed
      * to by `first'.
      * TODO This is quite weird, having the list function on the obj rather than
      * on the list...
      * @param <T> Type of the object/list
      * @param first List to which the object should be added.
      */
-    public <T extends DObject> void addObjectToList (Ref<T> first)
+    private <T extends DObject> void addObjectToList (Ref<T> first)
     {
-//      System.err.println("ADDING OBJ: " + obj.getClass().getName());
         //  obj.next = *first;
         //  obj.tome = first;
         //  if (first != null) first.tome = &obj.next;
         //  first = obj;
-        _next = first.get();
+        _next.set(first.get());
         _tome = (Ref<DObject>) first;
         if (first.get() != null) {
             first.get().setTome(_next);
@@ -89,39 +84,36 @@ public abstract class DObject extends DBase {
         first.set((T) this);
     }
 
-    protected void setTome(DObject obj) {
-        _tome.set(obj);
+    private void setTome(Ref<DObject> obj) {
+        _tome = obj;
     }
 
 
-	/**
-	 * Remove the object from the linked list.
-	 */
-//	public static <T extends dObject>void removeObjectFromList (T obj)
-	public <T extends DObject>void removeObjectFromList ()
-	{
-		//System.err.println("REMOVING OBJ: " + getClass().getName() + " / " + toString());
-		//	  if (obj.next) obj.next.tome = obj.tome;
-		//	  *(obj.tome) = obj.next;
-		//	  // safeguard
-		//	  obj.next = 0;
-		//	  obj.tome = 0;
-		if (_next != null) _next._tome = _tome;
-		_tome.set(_next);
-		// safeguard
-		_next = null;
-		_tome = null;
-	}
-	
-	public DxWorld getWorld() {
-		return world;
-	}
-	
-	public DObject getNext() {
-		return _next;
-	}
-	
-	protected DObject getTome() {
-		return _tome.get();
-	}
+    /**
+     * Remove the object from the linked list.
+     */
+//    public static <T extends dObject>void removeObjectFromList (T obj)
+    public <T extends DObject>void removeObjectFromList ()
+    {
+        //System.err.println("REMOVING OBJ: " + getClass().getName() + " / " + toString());
+        //      if (obj.next) obj.next.tome = obj.tome;
+        //      *(obj.tome) = obj.next;
+        //      // safeguard
+        //      obj.next = 0;
+        //      obj.tome = 0;
+        if (_next.get() != null) _next.get()._tome = _tome;
+        _tome.set(_next.get());
+        // safeguard
+        _next.set(null);
+        _tome = null;
+    }
+   
+    public DxWorld getWorld() {
+        return world;
+    }
+   
+    public DObject getNext() {
+        return _next.get();
+
+    }
 }
