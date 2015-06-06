9e867130bc7295adc00e493fd92a929e1ec17812
piotr <piotr@jtilia.org>
2015-06-06 17:36:06 +0200

    Add constrained ball joint with demo

---
+++ core/src/main/java/org/ode4j/ode/internal/joints/DxJointConstrainedBall.java
@@ -0,0 +1,141 @@
+/*************************************************************************
+ *                                                                       *
+ * Open Dynamics Engine, Copyright (C) 2001,2002 Russell L. Smith.       *
+ * All rights reserved.  Email: russ@q12.org   Web: www.q12.org          *
+ * Open Dynamics Engine 4J, Copyright (C) 2009-2014 Tilmann Zaeschke     *
+ * All rights reserved.  Email: ode4j@gmx.de   Web: www.ode4j.org        *
+ *                                                                       *
+ * This library is free software; you can redistribute it and/or         *
+ * modify it under the terms of EITHER:                                  *
+ *   (1) The GNU Lesser General Public License as published by the Free  *
+ *       Software Foundation; either version 2.1 of the License, or (at  *
+ *       your option) any later version. The text of the GNU Lesser      *
+ *       General Public License is included with this library in the     *
+ *       file LICENSE.TXT.                                               *
+ *   (2) The BSD-style license that is included with this library in     *
+ *       the file ODE-LICENSE-BSD.TXT and ODE4J-LICENSE-BSD.TXT.         *
+ *                                                                       *
+ * This library is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the files    *
+ * LICENSE.TXT, ODE-LICENSE-BSD.TXT and ODE4J-LICENSE-BSD.TXT for more   *
+ * details.                                                              *
+ *                                                                       *
+ *************************************************************************/
+package org.ode4j.ode.internal.joints;
+
+import static org.ode4j.ode.OdeMath.dCalcVectorCross3;
+import static org.ode4j.ode.internal.Common.M_PI;
+import static org.ode4j.ode.internal.Rotation.dRFromAxisAndAngle;
+
+import org.ode4j.math.DMatrix3;
+import org.ode4j.math.DVector3;
+import org.ode4j.ode.DWorld;
+import org.ode4j.ode.OdeMath;
+import org.ode4j.ode.internal.DxWorld;
+
+/**
+ * Ball and socket joint with constraints as described here http://www.monsterden.net/software/ragdoll-pyode-tutorial
+ */
+public class DxJointConstrainedBall extends DxJointBall {
+
+	private final DVector3 baseAxis;
+	private final DVector3 body2Axis;
+	private final DVector3 twistUpAxis1;
+	private final DVector3 twistUpAxis2;
+	private final DVector3 axis1;
+	private final DVector3 axis2;
+	private final DxJointLimitMotor limotFlex;
+	private final DxJointLimitMotor limotTwist;
+
+	public DxJointConstrainedBall(DWorld w) {
+		super((DxWorld) w);
+		baseAxis = new DVector3();
+		body2Axis = new DVector3();
+		twistUpAxis1 = new DVector3();
+		twistUpAxis2 = new DVector3();
+		limotFlex = new DxJointLimitMotor();
+		limotFlex.init((DxWorld) w);
+		limotTwist = new DxJointLimitMotor();
+		limotTwist.init((DxWorld) w);
+		axis1 = new DVector3();
+		axis2 = new DVector3();
+	}
+
+	@Override
+	public void getInfo1(DxJoint.Info1 info) {
+		info.setNub(3);
+		info.setM(3);
+		boolean limitingFlex = (limotFlex.lostop >= -M_PI || limotFlex.histop <= M_PI)
+				&& limotFlex.lostop <= limotFlex.histop;
+		boolean limitingTwist = (limotTwist.lostop >= -M_PI || limotTwist.histop <= M_PI)
+				&& limotTwist.lostop <= limotTwist.histop;
+		DVector3 flex = new DVector3();
+		getAxis(flex, baseAxis);
+		DVector3 twist = new DVector3();
+		getAxis2(twist, body2Axis);
+		dCalcVectorCross3(axis1, twist, flex);
+		axis1.safeNormalize();
+		double angle = limitingFlex ? Math.acos(flex.dot(twist)) : 0;
+		if (limotFlex.testRotationalLimit(angle)) {
+			info.incM();
+		}
+		if (limitingTwist) {
+			DVector3 twistUpB1 = new DVector3();
+			getAxis(twistUpB1, twistUpAxis1);
+			DVector3 twistUpB2 = new DVector3();
+			getAxis2(twistUpB2, twistUpAxis2);
+			DMatrix3 R = new DMatrix3();
+			dRFromAxisAndAngle(R, axis1, angle);
+			DVector3 projected = new DVector3();
+			OdeMath.dMultiply0_133(projected, twistUpB1, R);
+			double angle2 = Math.acos(projected.dot(twistUpB2));
+			if (limotTwist.testRotationalLimit(angle2)) {
+				dCalcVectorCross3(axis2, twistUpB2, projected);
+				axis2.safeNormalize();
+				info.incM();
+			}
+		} else {
+			limotTwist.testRotationalLimit(0.0);
+		}
+	}
+
+	@Override
+	public void getInfo2(double worldFPS, double worldERP, DxJoint.Info2Descr info) {
+		setBall(this, worldFPS, erp, info, anchor1, anchor2);
+		int m = 3;
+		m += limotFlex.addLimot(this, worldFPS, info, m, axis1, true);
+		m += limotTwist.addLimot(this, worldFPS, info, m, axis2, true);
+	}
+
+	/**
+	 * @param baseAxis - base axis for constraints
+	 * @param body2Axis - main axis of the 2nd body
+	 */
+	public void setAxes(DVector3 baseAxis, DVector3 body2Axis) {
+		baseAxis.safeNormalize();
+		body2Axis.safeNormalize();
+		DVector3 twistUpAxis = new DVector3();
+		if (Math.abs(body2Axis.dot(new DVector3(0.0, 1.0, 0.0))) < 0.7) {
+			twistUpAxis.set(0, 1, 0);
+		} else {
+			twistUpAxis.set(0, 0, 1);
+		}
+		DVector3 cross = new DVector3();
+		cross.eqCross(body2Axis, twistUpAxis);
+		cross.safeNormalize();
+		twistUpAxis.eqCross(cross, body2Axis);
+		twistUpAxis.safeNormalize();
+		setAxes(baseAxis, this.baseAxis, null);
+		setAxes(body2Axis, null, this.body2Axis);
+		setAxes(twistUpAxis, twistUpAxis1, twistUpAxis2);
+	}
+
+	public void setLimits(double flexLimit, double twistLimit) {
+		limotFlex.set(PARAM.dParamLoStop, -flexLimit);
+		limotFlex.set(PARAM.dParamHiStop, flexLimit);
+		limotTwist.set(PARAM.dParamLoStop, -twistLimit);
+		limotTwist.set(PARAM.dParamHiStop, twistLimit);
+	}
+
+}
---
+++ demo/src/main/java/org/ode4j/demo/DemoJointConstrainedBall.java
@@ -0,0 +1,191 @@
+/*************************************************************************
+ *                                                                       *
+ * Open Dynamics Engine, Copyright (C) 2001,2002 Russell L. Smith.       *
+ * All rights reserved.  Email: russ@q12.org   Web: www.q12.org          *
+ *                                                                       *
+ * This library is free software; you can redistribute it and/or         *
+ * modify it under the terms of EITHER:                                  *
+ *   (1) The GNU Lesser General Public License as published by the Free  *
+ *       Software Foundation; either version 2.1 of the License, or (at  *
+ *       your option) any later version. The text of the GNU Lesser      *
+ *       General Public License is included with this library in the     *
+ *       file LICENSE.TXT.                                               *
+ *   (2) The BSD-style license that is included with this library in     *
+ *       the file LICENSE-BSD.TXT.                                       *
+ *                                                                       *
+ * This library is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the files    *
+ * LICENSE.TXT and LICENSE-BSD.TXT for more details.                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ode4j.demo;
+
+import static org.ode4j.drawstuff.DrawStuff.dsDrawBox;
+import static org.ode4j.drawstuff.DrawStuff.dsDrawSphere;
+import static org.ode4j.drawstuff.DrawStuff.dsSetColorAlpha;
+import static org.ode4j.drawstuff.DrawStuff.dsSetTexture;
+import static org.ode4j.drawstuff.DrawStuff.dsSetViewpoint;
+import static org.ode4j.drawstuff.DrawStuff.dsSimulationLoop;
+
+import org.ode4j.drawstuff.DrawStuff.DS_TEXTURE_NUMBER;
+import org.ode4j.drawstuff.DrawStuff.dsFunctions;
+import org.ode4j.math.DMatrix3C;
+import org.ode4j.math.DVector3;
+import org.ode4j.math.DVector3C;
+import org.ode4j.ode.DBody;
+import org.ode4j.ode.DBox;
+import org.ode4j.ode.DGeom;
+import org.ode4j.ode.DMass;
+import org.ode4j.ode.DSpace;
+import org.ode4j.ode.DSphere;
+import org.ode4j.ode.DWorld;
+import org.ode4j.ode.OdeHelper;
+import org.ode4j.ode.internal.joints.DxJointConstrainedBall;
+
+public class DemoJointConstrainedBall extends dsFunctions {
+
+	private DWorld world;
+	private DSpace space;
+	private DBody body1;
+	private DBody body2;
+	private DxJointConstrainedBall joint1;
+	private DxJointConstrainedBall joint2;
+
+	private static double[] xyz = {3.8966, -2.0614, 4.0300};
+	private static double[] hpr = {153.5, -16.5, 0};
+
+	@Override
+	public void start()
+	{
+		world = OdeHelper.createWorld();
+		world.setGravity(0,0,-9.8);
+
+		world.setDamping(1e-4, 1e-5);
+		//    dWorldSetERP(world, 1);
+
+		space = OdeHelper.createSimpleSpace();
+
+		body1 = OdeHelper.createBody(world);
+		body2 = OdeHelper.createBody(world);
+
+		body1.setPosition(0, 0, 3);
+		body2.setPosition(0, 0, 2);
+
+
+		DGeom g;
+		DMass mass = OdeHelper.createMass();
+
+		g = OdeHelper.createBox(space, 0.2, 0.2, 1);
+		g.setBody(body1);
+		mass.setBox(1, 0.2, 0.2, 1);
+		body1.setMass(mass);
+
+		g = OdeHelper.createBox(space, 0.2, 0.2, 1);
+		g.setBody(body2);
+		mass.setBox(1, 0.2, 0.2, 1);
+		body2.setMass(mass);
+
+		joint1 = new DxJointConstrainedBall(world);
+		joint1.attach(body1, null);
+		joint1.setAnchor(0, 0, 3.5);
+		joint1.setAxes(new DVector3(0, 0, 1), new DVector3(0, 0, 1));
+		joint1.setLimits(0.35 * Math.PI, 0.3 * Math.PI);
+
+		joint2 = new DxJointConstrainedBall(world);
+		joint2.attach(body1, body2);
+		joint2.setAnchor(0, 0, 2.5);
+		joint2.setAxes(new DVector3(0, 0, 1), new DVector3(0, 0, 1));
+		joint2.setLimits(0.3 * Math.PI, 0.4 * Math.PI);
+
+		// initial camera position
+		dsSetViewpoint (xyz,hpr);
+	}
+
+	@Override
+	public void stop()
+	{
+		space.destroy();
+
+		world.destroy();
+	}
+
+
+	private void drawGeom(DGeom g)
+	{
+		//int gclass = dGeomGetClass(g);
+		DVector3C pos = g.getPosition();
+		DMatrix3C rot = g.getRotation();
+
+		if (g instanceof DSphere) {
+			dsSetColorAlpha(0, 0.75, 0.5, 1);
+			dsSetTexture (DS_TEXTURE_NUMBER.DS_CHECKERED);
+			dsDrawSphere(pos, rot, ((DSphere)g).getRadius());
+		} else if (g instanceof DBox) {
+			dsSetColorAlpha(1, 1, 0, 1);
+			dsSetTexture (DS_TEXTURE_NUMBER.DS_WOOD);
+			DVector3C lengths = ((DBox)g).getLengths();
+			dsDrawBox(pos, rot, lengths);
+		}
+	}
+
+	private static double t = 0;
+
+
+	@Override
+	public void step(boolean pause)
+	{
+		if (!pause) {
+
+
+			final double step = 0.005;
+			final int nsteps = 4;
+
+			for (int i=0; i<nsteps; ++i) {
+
+				double f = Math.sin(t*1.2)*0.8;
+				body1.addForceAtRelPos(
+						f, 0, 0, 
+						0.1, 0.1, -0.5); // at the lower end
+
+						double g = Math.sin(t*0.7)*0.8;
+						body2.addForceAtRelPos(
+								0.2 * g, g, 0, 
+								0.1, 0, -0.5); // at the lower end
+								t += step;
+
+								world.quickStep(step);
+			}
+		}
+
+		// now we draw everything
+		int ngeoms = space.getNumGeoms();
+		for (int i=0; i<ngeoms; ++i) {
+			DGeom g = space.getGeom(i);
+
+			drawGeom(g);
+		}
+
+	}
+
+
+	public static void main(String[] args) {
+		new DemoJointConstrainedBall().demo(args);
+	}
+
+	private void demo(String[] args) {
+		// create world
+		OdeHelper.initODE();
+
+		// run demo
+		dsSimulationLoop (args, 800, 600, this);
+
+		OdeHelper.closeODE();
+	}
+
+	@Override
+	public void command(char cmd) {
+		// Auto-generated method stub
+	}
+
+}
