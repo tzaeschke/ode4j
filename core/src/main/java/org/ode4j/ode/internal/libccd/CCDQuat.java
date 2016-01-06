64edf9a54c6b5d3e1d2418078d1db4a45c5b9abf
piotr <piotr@jtilia.org>
2016-01-06 09:26:59 +0100

    Optimize ccdQuatRotVec performance

--- core/src/main/java/org/ode4j/ode/internal/libccd/CCDQuat.java
+++ core/src/main/java/org/ode4j/ode/internal/libccd/CCDQuat.java
@@ -194,48 +194,29 @@ public class CCDQuat {
 
 
 
+
 	/**
 	 * Rotate vector v by quaternion q.
 	 */
 	public static final void ccdQuatRotVec(ccd_vec3_t v, final ccd_quat_t q)
 	{
-	    double w, x, y, z, ww, xx, yy, zz, wx, wy, wz, xy, xz, yz;
-	    double vx, vy, vz;
-
-	    w = q.q3;
-	    x = q.q0;
-	    y = q.q1;
-	    z = q.q2;
-	    ww = w*w;
-	    xx = x*x;
-	    yy = y*y;
-	    zz = z*z;
-	    wx = w*x;
-	    wy = w*y;
-	    wz = w*z;
-	    xy = x*y;
-	    xz = x*z;
-	    yz = y*z;
-
-	    vx = ww * ccdVec3X(v)
-	            + xx * ccdVec3X(v)
-	            - yy * ccdVec3X(v)
-	            - zz * ccdVec3X(v)
-	            + 2 * ((xy - wz) * ccdVec3Y(v)
-	            + (xz + wy) * ccdVec3Z(v));
-	    vy = ww * ccdVec3Y(v)
-	            - xx * ccdVec3Y(v)
-	            + yy * ccdVec3Y(v)
-	            - zz * ccdVec3Y(v)
-	            + 2 * ((xy + wz) * ccdVec3X(v)
-	            + (yz - wx) * ccdVec3Z(v));
-	    vz = ww * ccdVec3Z(v)
-	            - xx * ccdVec3Z(v)
-	            - yy * ccdVec3Z(v)
-	            + zz * ccdVec3Z(v)
-	            + 2 * ((xz - wy) * ccdVec3X(v)
-	            + (yz + wx) * ccdVec3Y(v));
-	    ccdVec3Set(v, vx, vy, vz);
+		// original version: 31 mul + 21 add
+		// optimized version: 18 mul + 12 add
+		// formula: v = v + 2 * cross(q.xyz, cross(q.xyz, v) + q.w * v)
+	    double x = q.q0;
+	    double y = q.q1;
+	    double z = q.q2;
+	    double w = q.q3;
+	    double vx = ccdVec3X(v);
+	    double vy = ccdVec3Y(v);
+	    double vz = ccdVec3Z(v);
+	    double cross1_x = y * vz - z * vy + w * vx;
+	    double cross1_y = z * vx - x * vz + w * vy;
+	    double cross1_z = x * vy - y * vx + w * vz;
+	    double cross2_x = y * cross1_z - z * cross1_y;
+	    double cross2_y = z * cross1_x - x * cross1_z;
+	    double cross2_z = x * cross1_y - y * cross1_x;
+        ccdVec3Set(v, vx + 2 * cross2_x, vy + 2 * cross2_y, vz + 2 * cross2_z);
 	}
 
 
---
+++ demo-cpp/src/test/java/org/ode4j/tests/libccd/TestQuat.java
@@ -0,0 +1,40 @@
+package org.ode4j.tests.libccd;
+
+import org.junit.Test;
+import org.ode4j.ode.internal.libccd.CCDQuat;
+import org.ode4j.ode.internal.libccd.CCDQuat.ccd_quat_t;
+import org.ode4j.ode.internal.libccd.CCDVec3.ccd_vec3_t;
+
+import static org.junit.Assert.*;
+
+public class TestQuat {
+
+	@Test
+	public void testRotation() {
+		ccd_quat_t q = new ccd_quat_t();
+		ccd_vec3_t a = new ccd_vec3_t(0, 1, 0);
+		CCDQuat.ccdQuatSetAngleAxis(q, Math.PI / 4, a);
+		ccd_vec3_t v = new ccd_vec3_t(1, 0, 0);
+		CCDQuat.ccdQuatRotVec(v, q);
+		assertEquals(Math.sqrt(2) / 2, v.get0(), 0.00001f);
+		assertEquals(-Math.sqrt(2) / 2, v.get2(), 0.00001f);
+		CCDQuat.ccdQuatSetAngleAxis(q, Math.PI / 6, a);
+		v = new ccd_vec3_t(1, 0, 0);
+		CCDQuat.ccdQuatRotVec(v, q);
+		assertEquals(Math.sqrt(3) / 2, v.get0(), 0.00001f);
+		assertEquals(-0.5, v.get2(), 0.00001f);
+		a.set(Math.sqrt(3), Math.sqrt(3), Math.sqrt(3));
+		CCDQuat.ccdQuatSetAngleAxis(q, Math.PI / 3, a);
+		v = new ccd_vec3_t(1, 1, 1);
+		CCDQuat.ccdQuatRotVec(v, q);
+		assertEquals(1.0, v.get0(), 0.00001f);
+		assertEquals(1.0, v.get1(), 0.00001f);
+		assertEquals(1.0, v.get2(), 0.00001f);
+		v = new ccd_vec3_t(1, 3, 1);
+		CCDQuat.ccdQuatSetAngleAxis(q, Math.PI / 6, a);
+		CCDQuat.ccdQuatRotVec(v, q);
+		assertEquals(0.511966612, v.get0(), 0.00001f);
+		assertEquals(2.821367205, v.get1(), 0.00001f);
+		assertEquals(1.666666666, v.get2(), 0.00001f);
+	}
+}
